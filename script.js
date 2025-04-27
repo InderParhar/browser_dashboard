62b29716845d8302605e4937943c7d5c99b4c863
caslox <inderparhar1997@gmail.com>
2025-04-27 04:55:55 -0400

    Added weather functionality

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const currentdate = new Date();
+
+var data;
+
 const date_element = document.getElementById("date_time");
+const weather_element = document.getElementById("weather");
 
-function refresh_window(){
-    window.setTimeout(function(){
+function refresh_window() {
+    window.setTimeout(function () {
         window.location.reload();
-    },10000)
+    }, 30000)
 }
 
-function update_DateTime(){
-date_element.innerHTML = "Date: " + currentdate.toLocaleDateString() + " Time: " + currentdate.toLocaleTimeString();
+//date and time
+function update_DateTime() {
+    date_element.innerHTML = "Date: " + currentdate.toLocaleDateString() + " Time: " + currentdate.toLocaleTimeString();
+}
+
+//weather
+async function get_weather() {
+    const response = await fetch(Weather_CONFIG.API_URL + "toronto" + "&appid=" + Weather_CONFIG.API_KEY + "&units=metric");
+
+    data = await (response).json();
+    weather_element.innerHTML = data.name + " temp: " + data.main.temp + "°c" + ", Airspace: " + data.weather[0].main;
+}
+
+//call functions
 refresh_window();
-}
-
 update_DateTime();
+get_weather();
